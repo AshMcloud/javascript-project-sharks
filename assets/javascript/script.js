613e5d3d06ee6f76fb7d754236e2c32a76ddb158
AshMcloud <ash.mcleod36@outlook.com>
2023-03-14 01:09:17 +0000

    navigating past the first question

--- assets/javascript/script.js
+++ assets/javascript/script.js
@@ -2,123 +2,81 @@ console.log('Hello world!');
 
 const startButton = document.getElementById('start');
 startButton.addEventListener('click', function() {
-    startGame()
+    firstQuestion()
 });
 
 /*Add actual question/answers within questions*/
-let questionOne = [
+let questions = [
     {
         question: "What depth range do you live at?",
         answerOne: "0-400m",
         answerTwo: "400m+"
     },
-];
-
-let questionTwoA = [
     {
         question: "What average water temperature do you prefer?",
         answerOne: "<20°C",
         answerTwo: "20°C +"
     },
-];
-
-let questionTwoB = [
     {
         question: "What average water temperature do you prefer?",
         answerOne: "<10°C",
         answerTwo: "10°C +"
     },
-];
-
-let questionThreeA = [
     {
         question: "How great is your size?",
         answerOne: "<3.3m",
         answerTwo: "<7.4m"
     },
-];
-
-let questionThreeB = [
     {
         question: "How great is your size?",
         answerOne: "<3.5m",
         answerTwo: "<6.1m"
     },
-];
-
-let questionThreeC = [
     {
         question: "How great is your size?",
         answerOne: "<730cm",
         answerTwo: "<196cm"
     },
-];
-
-let questionThreeD = [
     {
         question: "How great is your size?",
         answerOne: "<8m",
         answerTwo: "<21m"
     },
-];
-
-let questionFourA = [
     {
         question: "What is your prefered habitat?",
         answerOne: "Muddy/sandy banks",
         answerTwo: "Continental shelves/ Pelagic region"
     },
-];
-
-let questionFourB = [
     {
         question: "What is your prefered habitat?",
         answerOne: "Migratory",
         answerTwo: "Coastal/ Atoll Islands"
     },
-];
-
-let questionFourC = [
     {
         question: "What is your prefered habitat?",
         answerOne: "Tropic coral reefs",
         answerTwo: "Coral reefs/ Lagoons"
     },
-];
-
-let questionFourD = [
     {
         question: "What is your prefered habitat?",
         answerOne: "Rocky or coral shelves/Mangroves",
         answerTwo: "Coastal/ Pelagic region"
     },
-];
-
-let questionFourE = [
     {
         question: "What is your prefered habitat?",
         answerOne: "Deep continetal shelves",
         answerTwo: "North Atlantic/Arctic Sea"
     },
-];
-
-let questionFourF = [
     {
         question: "What is your prefered habitat?",
         answerOne: "Epibenthic/ Benthic/ Pelagic region",
         answerTwo: "Mesopelagic region"
     },
-];
-
-let questionFourG = [
     {
         question: "What is your prefered habitat?",
         answerOne: "Oceanic/ Epipelagic/ Open ocean",
         answerTwo: "Contintental Shelves"
     },
-];
-
-let questionFourH = [
     {
         question: "What is your prefered habitat?",
         answerOne: "Coastal/ Continental Shelves",
@@ -223,32 +181,56 @@ let results = [
     },
 ];
 
-
-/*I want to iterate through each question individually before moving onto the next*/
-function startGame() {
-let htmlQOne = `
+function firstQuestion(){
+    for (let question of questions) {
+    let htmlQOne = `
     <table class="center" id="quiz-display">
         <tr>
-            <th class="info-display">Question:</th>
+            <th>Question:</th>
+            <th>${question.question}</th>
         </tr>
-        `;
-for (let question of questionOne){
-    let firstQ = `
-    <tr>
-        <th>${question.question}</th>
-    </tr>
-    <tr>
-        <td>${question.answerOne}</td><button><i class="fa-solid fa-check"></i></button>
-        <td>${question.answerTwo}</td><button><i class="fa-solid fa-check"></i></button>
-    </tr>`;
-htmlQOne += firstQ
+        <tr>
+            <td>${question.answerOne}<button id="aOne" type="button" onclick="secondQuestion()"><i class="fa-solid fa-check"></i></button></td>
+            <td>${question.answerTwo}<button id="aTwo" type="button" onclick="thirdQuestion()"><i class="fa-solid fa-check"></i></button></td>
+        </tr>
+    </table>`;
+    document.getElementById('quiz-area').innerHTML = htmlQOne;
+    return htmlQOne;
+    } 
 }
-htmlQOne += `
-</table>
-`;
-return htmlQOne;
 
-document.getElementsByClassName('quiz-area').innerHTML = htmlQOne;
-/** buttons on click move to new fuction for next question*/
+function secondQuestion() {
+    for (let question of questions) {
+        let htmlQOne = `
+        <table class="center" id="quiz-display">
+            <tr>
+                <th>Question:</th>
+                <th>${question.question[1]}</th>
+            </tr>
+            <tr>
+                <td>${question.answerOne[1]}<button id="aOne" type="button"><i class="fa-solid fa-check"></i></button></td>
+                <td>${question.answerTwo[1]}<button id="aTwo" type="button"><i class="fa-solid fa-check"></i></button></td>
+            </tr>
+        </table>`;
+        document.getElementById('quiz-area').innerHTML = htmlQOne;
+        return htmlQOne;
+        } 
 }
-/*after all answers tallied I want to run a new fuction for the results*/
+
+function thirdQuestion() {
+    for (let question of questions) {
+        let htmlQOne = `
+        <table class="center" id="quiz-display">
+            <tr>
+                <th>Question:</th>
+                <th>${question.question[2]}</th>
+            </tr>
+            <tr>
+                <td>${question.answerOne[2]}<button id="aOne" type="button"><i class="fa-solid fa-check"></i></button></td>
+                <td>${question.answerTwo[2]}<button id="aTwo" type="button"><i class="fa-solid fa-check"></i></button></td>
+            </tr>
+        </table>`;
+        document.getElementById('quiz-area').innerHTML = htmlQOne;
+        return htmlQOne;
+        } 
+}
