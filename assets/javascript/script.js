44ab7798b3e061a84c452c2e67ddd4651a94d7ae
AshMcloud <ash.mcleod36@outlook.com>
2023-03-14 03:50:07 +0000

    completely restructuring the questions array

--- assets/javascript/script.js
+++ assets/javascript/script.js
@@ -9,176 +9,303 @@ startButton.addEventListener('click', function() {
 let questions = [
     {
         question: "What depth range do you live at?",
-        answerOne: "0-400m",
-        answerTwo: "400m+"
+        answerOne: {
+            text: "0-400m",
+            angel: 1,
+            blue: 1,
+            thresher: 1,
+            tiger: 1,
+            blackTip: 1,
+            zebra: 1,
+            nurse: 1,
+            gHammer: 1,
+            goblin: 0,
+            greenland: 0,
+            frilled: 0,
+            pygmy: 0,
+            oceanic: 0,
+            gWhite: 0,
+            basking: 0,
+            whale: 0,
+        },
+        answerTwo: {
+            text: "400m+",
+            angel: 0,
+            blue: 0,
+            thresher: 0,
+            tiger: 0,
+            blackTip: 0,
+            zebra: 0,
+            nurse: 0,
+            gHammer: 0,
+            goblin: 1,
+            greenland: 1,
+            frilled: 1,
+            pygmy: 1,
+            oceanic: 1,
+            gWhite: 1,
+            basking: 1,
+            whale: 1,
+        }
     },
     {
         question: "What average water temperature do you prefer?",
-        answerOne: "<20°C",
-        answerTwo: "20°C +"
-    },
-    {
-        question: "What average water temperature do you prefer?",
-        answerOne: "<10°C",
-        answerTwo: "10°C +"
-    },
-    {
-        question: "How great is your size?",
-        answerOne: "<3.3m",
-        answerTwo: "<7.4m"
-    },
-    {
-        question: "How great is your size?",
-        answerOne: "<3.5m",
-        answerTwo: "<6.1m"
-    },
-    {
-        question: "How great is your size?",
-        answerOne: "<730cm",
-        answerTwo: "<196cm"
-    },
-    {
-        question: "How great is your size?",
-        answerOne: "<8m",
-        answerTwo: "<21m"
-    },
-    {
-        question: "What is your prefered habitat?",
-        answerOne: "Muddy/sandy banks",
-        answerTwo: "Continental shelves/ Pelagic region"
-    },
-    {
-        question: "What is your prefered habitat?",
-        answerOne: "Migratory",
-        answerTwo: "Coastal/ Atoll Islands"
-    },
-    {
-        question: "What is your prefered habitat?",
-        answerOne: "Tropic coral reefs",
-        answerTwo: "Coral reefs/ Lagoons"
-    },
-    {
-        question: "What is your prefered habitat?",
-        answerOne: "Rocky or coral shelves/Mangroves",
-        answerTwo: "Coastal/ Pelagic region"
-    },
-    {
-        question: "What is your prefered habitat?",
-        answerOne: "Deep continetal shelves",
-        answerTwo: "North Atlantic/Arctic Sea"
-    },
-    {
-        question: "What is your prefered habitat?",
-        answerOne: "Epibenthic/ Benthic/ Pelagic region",
-        answerTwo: "Mesopelagic region"
-    },
-    {
-        question: "What is your prefered habitat?",
-        answerOne: "Oceanic/ Epipelagic/ Open ocean",
-        answerTwo: "Contintental Shelves"
+        answerOne: { 
+            text: "<10°C",
+            angel: 0,
+            blue: 0,
+            thresher: 0,
+            tiger: 0,
+            blackTip: 0,
+            zebra: 0,
+            nurse: 0,
+            gHammer: 0,
+            goblin: 1,
+            greenland: 1,
+            frilled: 1,
+            pygmy: 1,
+            oceanic: 0,
+            gWhite: 0,
+            basking: 0,
+            whale: 0,
+        },
+        answerTwo: {
+            text: "10°C - 20°C",
+            angel: 1,
+            blue: 1,
+            thresher: 1,
+            tiger: 0,
+            blackTip: 0,
+            zebra: 0,
+            nurse: 0,
+            gHammer: 0,
+            goblin: 0,
+            greenland: 0,
+            frilled: 0,
+            pygmy: 0,
+            oceanic: 0,
+            gWhite: 1,
+            basking: 1,
+            whale: 0,
+        },
+        answerThree: {
+            text: "20°C+",
+            angel: 0,
+            blue: 0,
+            thresher: 0,
+            tiger: 1,
+            blackTip: 1,
+            zebra: 1,
+            nurse: 1,
+            gHammer: 1,
+            goblin: 0,
+            greenland: 0,
+            frilled: 0,
+            pygmy: 0,
+            oceanic: 1,
+            gWhite: 0,
+            basking: 0,
+            whale: 1,
+        }
     },
     {
         question: "What is your prefered habitat?",
-        answerOne: "Coastal/ Continental Shelves",
-        answerTwo: "Open ocean/ Pelagic region"
-    },
-];
+        answerOne: {
+            text: "Continental shelves",
+            angel: 0,
+            blue: 1,
+            thresher: 0,
+            tiger: 0,
+            blackTip: 0,
+            zebra: 0,
+            nurse: 0,
+            gHammer: 0,
+            goblin: 1,
+            greenland: 0,
+            frilled: 0,
+            pygmy: 0,
+            oceanic: 0,
+            gWhite: 1,
+            basking: 1,
+            whale: 0,
+        },
+        answerTwo: {
+            text: "Coral reefs or lagoons",
+            angel: 0,
+            blue: 0,
+            thresher: 0,
+            tiger: 0,
+            blackTip: 1,
+            zebra: 1,
+            nurse: 1,
+            gHammer: 0,
+            goblin: 0,
+            greenland: 0,
+            frilled: 0,
+            pygmy: 0,
+            oceanic: 0,
+            gWhite: 0,
+            basking: 0,
+            whale: 0,
+        },
+        answerThree: {
+            text: "Sandy/muddy banks, atolls or mangroves",
+            angel: 1,
+            blue: 0,
+            thresher: 0,
+            tiger: 1,
+            blackTip: 0,
+            zebra: 0,
+            nurse: 1,
+            gHammer: 0,
+            goblin: 0,
+            greenland: 0,
+            frilled: 0,
+            pygmy: 0,
+            oceanic: 0,
+            gWhite: 0,
+            basking: 0,
+            whale: 0,
 
-let results = [
-    {
-        result: "Angel Shark",
-        fact: " ",
-        Image: " "
+        },
+        answerFour: {
+            text: "Pelagic/Open ocean",
+            angel: 0,
+            blue: 1,
+            thresher: 1,
+            tiger: 0,
+            blackTip: 0,
+            zebra: 0,
+            nurse: 0,
+            gHammer: 1,
+            goblin: 0,
+            greenland: 0,
+            frilled: 0,
+            pygmy: 0,
+            oceanic: 1,
+            gWhite: 1,
+            basking: 0,
+            whale: 1,
+        
+        },
+        answerFive: {
+            text: "Deep, dark & cold!",
+            angel: 0,
+            blue: 0,
+            thresher: 0,
+            tiger: 0,
+            blackTip: 0,
+            zebra: 0,
+            nurse: 0,
+            gHammer: 0,
+            goblin: 1,
+            greenland: 1,
+            frilled: 1,
+            pygmy: 1,
+            oceanic: 0,
+            gWhite: 0,
+            basking: 0,
+            whale: 0,
+        },
     },
     {
-        result: "Blue Shark",
-        fact: " ",
-        Image: " "
-
-    },
-    {
-        result: "Thresher Shark",
-        fact: " ",
-        Image: " "
-
-    },
-    {
-        result: "Tiger Shark",
-        fact: " ",
-        Image: " "
-    },
-    {
-        result: "Black Tip Reef Shark",
-        fact: " ",
-        Image: " "
-
-    },
-    {
-        result: "Zebra Shark",
-        fact: " ",
-        Image: " "
-
-    },
-    {
-        result: "Nursing Shark",
-        fact: " ",
-        Image: " "
-
-    },
-    {
-        result: "Great Hammered Head",
-        fact: " ",
-        Image: " "
-
-    },
-    {
-        result: "Goblin Shark",
-        fact: " ",
-        Image: " "
-
-    },
-    {
-        result: "Greenland Shark",
-        fact: " ",
-        Image: " "
-
-    },
-    {
-        result: "Frilled Shark",
-        fact: " ",
-        Image: " "
-
-    },
-    {
-        result: "Pygmy Shark",
-        fact: " ",
-        Image: " "
-
-    },
-    {
-        result: "Oceanic White Tip Shark",
-        fact: " ",
-        Image: " "
-
-    },
-    {
-        result: "Great White Shark",
-        fact: " ",
-        Image: " "
-
-    },
-    {
-        result: "Basking Shark",
-        fact: " ",
-        Image: " "
-
-    },
-    {
-        result: "Whale Shark",
-        fact: " ",
-        Image: " "
-
-    },
+        question : "As a mature shark, what is your average size?",
+        answerOne: {
+            text: "<1m",
+            angel: 0,
+            blue: 0,
+            thresher: 0,
+            tiger: 0,
+            blackTip: 0,
+            zebra: 0,
+            nurse: 0,
+            gHammer: 0,
+            goblin: 1,
+            greenland: 1,
+            frilled: 1,
+            pygmy: 1,
+            oceanic: 0,
+            gWhite: 0,
+            basking: 0,
+            whale: 0,
+        },
+        answerTwo : {
+            text: "1m-3m",
+            angel: 1,
+            blue: 1,
+            thresher: 0,
+            tiger: 0,
+            blackTip: 1,
+            zebra: 0,
+            nurse: 0,
+            gHammer: 0,
+            goblin: 0,
+            greenland: 0,
+            frilled: 0,
+            pygmy: 0,
+            oceanic: 0,
+            gWhite: 0,
+            basking: 0,
+            whale: 0,
+        },
+        answerThree : {
+            text: "2.5m-5m",
+            angel: 0,
+            blue: 0,
+            thresher: 0,
+            tiger: 0,
+            blackTip: 0,
+            zebra: 1,
+            nurse: 1,
+            gHammer: 0,
+            goblin: 0,
+            greenland: 0,
+            frilled: 0,
+            pygmy: 0,
+            oceanic: 1,
+            gWhite: 0,
+            basking: 0,
+            whale: 0,
+        },
+        answerFour : {
+            text: "5m-7.5m",
+            angel: 0,
+            blue: 0,
+            thresher: 1,
+            tiger: 1,
+            blackTip: 0,
+            zebra: 0,
+            nurse: 0,
+            gHammer: 1,
+            goblin: 0,
+            greenland: 0,
+            frilled: 0,
+            pygmy: 0,
+            oceanic: 0,
+            gWhite: 0,
+            basking: 0,
+            whale: 0,
+        },
+        answerFive : {
+            text: "7.5m+",
+            angel: 0,
+            blue: 0,
+            thresher: 0,
+            tiger: 0,
+            blackTip: 0,
+            zebra: 0,
+            nurse: 0,
+            gHammer: 0,
+            goblin: 0,
+            greenland: 0,
+            frilled: 0,
+            pygmy: 0,
+            oceanic: 0,
+            gWhite: 1,
+            basking: 1,
+            whale: 1,
+            },
+        }
 ];
 
 function firstQuestion(){
@@ -197,40 +324,4 @@ function firstQuestion(){
     document.getElementById('quiz-area').innerHTML = htmlQOne;
     return htmlQOne;
     } 
-}
-
-function secondQuestion() {
-    for (let question of questions) {
-        let htmlQOne = `
-        <table class="center" id="quiz-display">
-            <tr>
-                <th>Question:</th>
-                <th>${question.question[1]}</th>
-            </tr>
-            <tr>
-                <td>${question.answerOne[1]}<button id="aOne" type="button"><i class="fa-solid fa-check"></i></button></td>
-                <td>${question.answerTwo[1]}<button id="aTwo" type="button"><i class="fa-solid fa-check"></i></button></td>
-            </tr>
-        </table>`;
-        document.getElementById('quiz-area').innerHTML = htmlQOne;
-        return htmlQOne;
-        } 
-}
-
-function thirdQuestion() {
-    for (let question of questions) {
-        let htmlQOne = `
-        <table class="center" id="quiz-display">
-            <tr>
-                <th>Question:</th>
-                <th>${question.question[2]}</th>
-            </tr>
-            <tr>
-                <td>${question.answerOne[2]}<button id="aOne" type="button"><i class="fa-solid fa-check"></i></button></td>
-                <td>${question.answerTwo[2]}<button id="aTwo" type="button"><i class="fa-solid fa-check"></i></button></td>
-            </tr>
-        </table>`;
-        document.getElementById('quiz-area').innerHTML = htmlQOne;
-        return htmlQOne;
-        } 
 }
