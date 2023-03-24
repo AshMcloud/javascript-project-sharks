74c8e1a0355cd378c83c84c1c7a112640ea9a4d7
AshMcloud <ash.mcleod36@outlook.com>
2023-03-24 04:41:59 +0000

    click alert added to answer buttons

--- assets/javascript/script2.js
+++ assets/javascript/script2.js
@@ -1,6 +1,6 @@
 const question = [
     {
-        ask : "What depth range do you live at?",
+        ask : "What ocean depth range do you live at?",
         answer : [
             {
                 text : "0-400m",
@@ -21,81 +21,26 @@ const question = [
         ]
     },
     {
-            ask : "What average water temperature do you prefer?",
-            answer : {
+        ask : "What average water temperature do you prefer?",
+        answer : [
+            {
                 text : "<10°C",
                 score : ["goblin", "greenland", "frilled", "pygmy"]
             },
-            answer : {
+            {
                 text : "10°C - 20°C",
                 score : ["angel", "blue", "thresher", "gWhite", "basking"]
             },
-            answer : {
+            {
                 text : "20°C+",
                 score : ["tiger", "blackTip", "zebra", "nurse", "gHammer", "oceanic", "whale"]
             },
-            answer : {
-                text : " ",
-                score : []
-            }
-            },
-        {
-                ask : " ",
-                answer : {
-                    text : " ",
-                    score : []
-                },
-                answer : {
-                    text : " ",
-                    score : []
-                },
-                answer : {
-                    text : " ",
-                    score : []
-                },
-                answer : {
-                    text : " ",
-                    score : []
-                }
-                },
             {
-                    ask : " ",
-                    answer : {
-                        text : " ",
-                        score : []
-                    },
-                    answer : {
-                        text : " ",
-                        score : []
-                    },
-                    answer : {
-                        text : " ",
-                        score : []
-                    },
-                    answer : {
-                        text : " ",
-                        score : []
-                    }
-                    },
-                {
-                        ask : " ",
-                        answer : {
-                            text : " ",
-                            score : []
-                        },
-                        answer : {
-                            text : " ",
-                            score : []
-                        },
-                        answer : {
-                            text : " ",
-                            score : []
-                        },
-                        answer : {
-                            text : " ",
-                            score : []
-                        }
-                        }
+                text : "eh",
+                score : [" "]
+            }
+        ]
+    },
 ];
 
 let begin = document.getElementById('start');
@@ -110,11 +55,30 @@ let firstAnswer = document.getElementById('answer-one').innerHTML = question[0].
 let secondAnswer = document.getElementById('answer-two').innerHTML = question[0].answer[1].text;
 let thirdAnswer = document.getElementById('answer-three').innerHTML = question[0].answer[2].text;
 let fourthAnswer = document.getElementById('answer-four').innerHTML = question[0].answer[3].text;
-
 };
 
-function recordScore() {
+let buttonOne = document.getElementById('answer-one');
+buttonOne.addEventListener("click", function(){
+    getScore()
+});
 
+let buttonTwo = document.getElementById('answer-two');
+buttonTwo.addEventListener("click", function(){
+    getScore()
+});
+
+let buttonThree = document.getElementById('answer-three');
+buttonThree.addEventListener("click", function(){
+    getScore()
+});
+
+let buttonFour = document.getElementById('answer-four');
+buttonFour.addEventListener("click", function(){
+    getScore()
+});
+
+function getScore() {
+alert("clicked")
 };
 
 function nextQuestion(){
