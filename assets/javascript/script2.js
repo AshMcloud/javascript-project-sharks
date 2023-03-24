8aee4e669cc357bd6b43706318859ea1f327c979
AshMcloud <ash.mcleod36@outlook.com>
2023-03-24 02:56:25 +0000

    showQuestions function displays question and answers!

--- assets/javascript/script2.js
+++ assets/javascript/script2.js
@@ -1,22 +1,24 @@
 const question = [
     {
         ask : "What depth range do you live at?",
-        answer : {
-            text : "0-400m",
-            score : ["angel", "blue", "thresher", "blackTip", "zebra", "nurse", "gHammer"]
-        },
-        answer : {
-            text : "400m+",
-            score : ["goblin", "greenland", "frilled", "pygmy", "oceanic", "gWhite", "basking", "whale"]
-        },
-        answer : {
-            text : " ",
-            score : []
-        },
-        answer : {
-            text : " ",
-            score : []
-        }
+        answer : [
+            {
+                text : "0-400m",
+                score : ["angel", "blue", "thresher", "blackTip", "zebra", "nurse", "gHammer"]
+            },
+            {
+                text : "400m+",
+                score : ["goblin", "greenland", "frilled", "pygmy", "oceanic", "gWhite", "basking", "whale"]
+            },
+            {
+                text : "ah",
+                score : [" "]
+            },
+            {
+                text : "eh",
+                score : [" "]
+            }
+        ]
     },
     {
             ask : "What average water temperature do you prefer?",
@@ -104,6 +106,10 @@ begin.addEventListener("click", function(){
 
 function showQuestions() {
 let questionArea = document.getElementById('ask-area').innerHTML = question[0].ask;
+let firstAnswer = document.getElementById('answer-one').innerHTML = question[0].answer[0].text;
+let secondAnswer = document.getElementById('answer-two').innerHTML = question[0].answer[1].text;
+let thirdAnswer = document.getElementById('answer-three').innerHTML = question[0].answer[2].text;
+let fourthAnswer = document.getElementById('answer-four').innerHTML = question[0].answer[3].text;
 
 };
 
